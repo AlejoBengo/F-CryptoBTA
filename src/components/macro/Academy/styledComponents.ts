90b019ae630a450da4f7fe33481a2ce01bf790b9
AlejoBengo <henryaskhenry@gmail.com>
2022-06-30 11:51:56 -0300

    config

--- src/components/macro/Academy/styledComponents.ts
+++ src/components/macro/Academy/styledComponents.ts
@@ -1,6 +1,5 @@
 import { Box, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
-<<<<<<< HEAD
 // import bkg from './bkg.png';
 /*----------------------------------------------------------------------------*/
 
@@ -82,89 +81,6 @@ export const Paragraph = styled(Box)`
 export const WhiteText = styled(Typography)`
    font-size: 1.2vw;
    font-weight: bold;
-=======
-import bkg from './bkg.png';
-import cursoGratis from './cursoGratis.png';
-import uno from "./1.png";
-import dos from "./BTARecurso 1.png";
-/*----------------------------------------------------------------------------*/
-
-export const Container = styled(Box)`
-width: 100%;
-height: 100%;
-min-height: 100%;
-   display: flex;
-   flex-direction: row;
-   flex-wrap: wrap;
-   align-content: flex-end;
-   justify-content: center;
-   align-items: baseline;
-   background-image: URL(${uno});
-   background-repeat: no-repeat;
-   background-position: center;
-   background-size: cover;
-   boder:5px solid #000;
-   `;
-export const Paraph = styled(Box)`
-display: flex;
-width:auto;
-   flex-direction: row;
-   flex-wrap: wrap;
-   justify-content: center;
-   align-items: center;
-`;
-
-export const ImgContainer = styled(Box)`
-   height: 100vh;
-   width:100%;
-   background-image: URL(${dos});
-   background-repeat: no-repeat;
-   background-position: center;
-   background-size: contain;
-   display: flex;
-    align-content: flex-end;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    `;
-
-    export const SubContainer = styled(Box)`
-    height: auto;
-    width:90%;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-content: space-between;
-    justify-content: flex-start;
-    align-items: center;
-    background-color: #00000000;
-   
-`;
-
-export const Input = styled('input')`
-   margin:5px 0px;
-   width: 100%;
-   height: 50px;
-   padding-text-left: 1rem;
-   border: 0.1rem solid white;
-   border-radius: 7% 7% 7% 7% / 50% 50% 50% 50%;
-   padding-left: 0.5rem;
-   align-self: center;
-   align-content: center;
-   font-weight: 900;
-   font-size: large;
-   background-color: #d9d9d9;
-   color: #00c1cb;
-   ::placeholder {
-      color: #00c1cb;
-      font-size: 1em;
-   }
-`;
-export const TextWhite = styled(Typography)`
-
-   font-weight: 900;
->>>>>>> 9d4210b1a2daf7a230ae1e84d84507494ba5bbeb
    color: #fff;
    margin-right: 0.4rem;
 
@@ -177,7 +93,6 @@ export const BlueText = styled(Typography)`
    font-size: 1.2vw;
    font-weight: bold;
    color: #00c1cb;
-<<<<<<< HEAD
    margin-right: 0.4rem;
 
    @media (max-width: 768px) {
@@ -191,23 +106,6 @@ export const ListBox = styled(Box)`
 
 export const LiBox = styled(Box)`
    display: flex;
-=======
-   display:inline;
-   margin: 0px ;
-   
-`;
-export const TextBlueBtn = styled(Typography)`
-   width: 100%;
-   height: auto;
-   border: 0.1rem solid white;
-   background-color: #00000000;
-   align-self: center;
-   align-content: center;
-   color: #00c1cb;
-   display:inline;
-   margin: 0px 0px 0px 0px;
-   
->>>>>>> 9d4210b1a2daf7a230ae1e84d84507494ba5bbeb
 `;
 
 export const Li = styled('li')`
